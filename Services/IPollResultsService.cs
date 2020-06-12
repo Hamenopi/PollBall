9844098fe57a02649df84dae25da9df591c7aab2
Justin Hamilton <JHamilton@Pas.com>
2020-06-12 15:41:02 -0500

    End Task 3

---
+++ Services/IPollResultsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PollBall.Services
+{
+    public interface IPollResultsService
+    {
+        public void AddVote(SelectedGame game);
+        public SortedDictionary<SelectedGame, int> GetVoteResult();
+    }
+}
